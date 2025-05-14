233ce898a857da8955ab936e3c890d994f891b1e
admin <admin@DESKTOP-5K1KTVU>
2025-05-14 17:19:47 +0900

    2025051441720

---
+++ src/com/yedam/Comments.java
@@ -0,0 +1,69 @@
+package com.yedam;
+
+import java.util.Scanner;
+
+
+// java로 끝나는 파일을 저장 및 컴파일하면 class라는 파일로 변환되어 실행
+public class Comments {
+	
+	public static void main(String[] args) {
+		
+		// 실행시 프로그램 메소드 중 "main"을 가장 먼저 찾고 실행!
+		
+		
+		
+		// 변수 데이터 타입
+		// int : 정수
+		// double : 실수
+		// String : 문자"열" 참조형 변수
+		// char : 문자
+		
+		
+		// 자바스크립트랑 다르게 변수 마다 데이터 형에 맞는 변수 선언해야함
+		
+		int int_a = 5; // 정수형 변수 선언(초기화) 방법
+		
+		double double_b = 2.4; // 정수형 변수 선언(초기화) 방법
+		
+		String str_c = new String("메렁"); // 문자'열' 저장
+		
+		char char_d = 'A'; // 문자 저장
+		
+		System.out.println(); // 자바스크립트의 console.log(); (출력하는 함수)		
+		// !!!!!!!!!!!!!!!!!!!!!!
+		// String 변수 str_c를 출력하면 "메렁" 값이 나오지만 비교할 때엔 변수가 저장된 주소값이 나오므로
+		// 문자열 끼리 비교할 땐 아래와 같이
+		// 비교할 데이터.equals(비교할 데이터2);
+		
+		// 데이터가 자동으로 형변환 되는 것을 보고 Promotion 이라고 부른다.
+		
+		double test_double = 2.1;
+		int test_int = 1;
+		double result = test_double + test_int;
+		System.out.println(result); // result = 3.1  # int가 자동으로 실수 (double) 형태로 자동 형변환
+		String test_str = new String("바보");
+		System.out.println("바보" + test_double + test_int); // double 형과 int 형 데이터들이 문자들과 연산을 하게되면 문자형태로 변환
+		System.out.println("바보" + (test_double + test_int)); // 자동 형변환을 막기 위해 괄호로 선위 연산하면 후에 문자랑 연산
+		
+		
+		
+		
+		// 데이터를 강제로 형변환 하는 것을 보고 Cascading 이라고 부른다.
+		int result2 = (int) test_double + test_int;
+		System.out.println(result2); // result = 3 # test_double 변수를 강제로 정수(int) 형태로 변환
+		
+
+		
+		//숫자를 문자로 바꾸기
+		System.out.println(result2 + "");
+		
+		
+		Scanner scn = new Scanner(System.in); // 자바스크립트의 prompt(); (입력하는 함수)
+		String value = scn.nextLine(); // 실질적으로 입력을 받아 value 변수에 저장하는 부분
+		
+		// !!!!!!!!!!!!!!!!!!!!!!
+		// Scanner 사용시 package 아래 java.util.Scanner을 import하여 줘야함
+	
+	} 
+
+}
--- src/com/yedam/JSExe.java
+++ src/com/yedam/JSExe.java
@@ -1,12 +1,79 @@
 package com.yedam;
 
+import java.util.Scanner;
+
 public class JSExe {
 	public static void main(String[] args) {
-		int sum = 0;
-		for (int cnt = 0; cnt <= 5; cnt++) {
-			int randomNum = (int) ((Math.random() * 10) + 1);
-			sum += randomNum;
+		
+		
+//		// promotion, cascating
+//		
+//		int num1 = 30;
+//		int num2 = 30;
+//		
+//		String str1 = new String("Hello");
+//		String str2 = new String("Hello");
+//		
+//		char char1 = 'H';
+//		char char2 = 'H';
+//		
+//		System.out.println(char1 == char2 );
+//		
+//		System.out.println(num1 == num2);
+//		System.out.println(str1.equals(str2)); // 문자열 변수의 데이터값 비교시 "변수명.equals(비교하려는 변수명)
+//		// 문자열 String은 참조 데이터
+		
+//		String str3 = "30";
+//		int num3 = 30;
+//		
+//		System.out.println(str3.equals("" + num3)); // 연산을 위해 promotion 
+//		int num4 = Integer.parseInt(str3); // cascating?
+//		
+//		System.out.println(num3 == num4);
+//		
+		test4();
+	}
+	
+	public static void test4() {
+		Scanner input = new Scanner(System.in);
+		while (true) {
+			System.out.println("메시지 입력, 죵료 = quit >>");
+			String msg = input.nextLine();
+			if (msg.equals("quit")) {
+				break;
+			} else {
+				System.out.println(msg);
+			}
 		}
-		System.out.println(sum);
+		System.out.println("end of prog.");
+		
+	}
+	
+	public static void test3() {
+
+		Scanner scn = new Scanner(System.in);
+		int i;
+		int sum = 0;
+		for (i = 0; i < 3; i++) {
+			System.out.println("학생의 점수를 입력 >> ");
+			String value = scn.nextLine();
+			int score = Integer.parseInt(value);
+			sum += score;
+		}
+		double avg = sum / i;
+		avg = Math.round(avg * 100) / 100.0;
+	}
+	
+	public static void test2() {
+
+		int sum = 0;
+		int cnt;
+		for (cnt = 0; cnt < 7; cnt++) {
+			int randomNum = (int) (Math.random() * 71) + 30;
+			sum += randomNum;
+			System.out.println(randomNum);
+		}
+		double result = Math.round(((double) sum / cnt) * 100) / 100.0;
+		System.out.println(result);
 	}
 }

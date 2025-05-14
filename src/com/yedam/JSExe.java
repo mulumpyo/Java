5758522e5bc864240d6a6441a287c6129f427f9b
admin <admin@DESKTOP-5K1KTVU>
2025-05-14 12:46:55 +0900

    Second Commit

--- src/com/yedam/JSExe.java
+++ src/com/yedam/JSExe.java
@@ -2,6 +2,11 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		
+		int sum = 0;
+		for (int cnt = 0; cnt <= 5; cnt++) {
+			int randomNum = (int) ((Math.random() * 10) + 1);
+			sum += randomNum;
+		}
+		System.out.println(sum);
 	}
 }
